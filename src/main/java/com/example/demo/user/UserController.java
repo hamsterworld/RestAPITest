34d63a5bbd045e6bafcd47e9c719842bd37fc462
hamster <hamster@hamster.com>
2022-11-06 22:35:34 +0900

    section4 completed

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,13 +1,22 @@
 package com.example.demo.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,15 +29,32 @@ public class UserController {
         return service.findAll();
     }
 
+//    @GetMapping("/users/{id}")
+//    public User retrieveUser(@PathVariable int id){
+//        User user = service.findOne(id);
+//
+//        if( user == null ){
+//            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+//        }
+//
+//        return user;
+//    }
+
+    //이거 HATEOAS에서 JsonFilter 어떻게하는겨
+    //client 입장에서는 추가다른 api를 제공받아서 뭘할수있는지 알수있는 대단히큰 장점이있다.
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = service.findOne(id);
 
         if( user == null ){
             throw new UserNotFoundException(String.format("ID[%s] not found",id));
         }
 
-        return user;
+        EntityModel<User> model = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+        return model;
     }
 
     @PostMapping("/users")
